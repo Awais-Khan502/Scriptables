a8926aaa5c97de805c0a975315632d8dfed78245
AwaisKhan <awaisnoshad.502@gmail.com>
2026-05-19 18:21:53 +0500

    ILoad value and ISaveValue Interfaces defined

---
+++ Assets/Variables/Interfaces/ILoadValue.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface ILoadValue<T>
+{
+    public T LoadValue();
+}
---
+++ Assets/Variables/Interfaces/ISaveValue.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface ISaveValue<T>
+{
+    public void SaveValue();
+}
